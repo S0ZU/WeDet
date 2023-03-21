a3bc3fe74942e41658103ef37819addeff0bdd27
Pavith Yatawara <38225598+S0ZU@users.noreply.github.com>
2023-03-21 14:06:07 +0530

    Update App.js

--- WeDet-frontend/App.js
+++ WeDet-frontend/App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+//import your screen
+import AppContainer from './src/screens/Home/HomeScreen';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! sadsadsad</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <AppContainer />
+ );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
