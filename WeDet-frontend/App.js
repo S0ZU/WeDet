95edfec2fc332b73053abce668cdb085cb170bb8
NickolaiTheek <73122334+NickolaiTheek@users.noreply.github.com>
2023-04-06 00:23:56 +0530

    Product screen

--- WeDet-frontend/App.js
+++ WeDet-frontend/App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 //import your screen
-import Home from './src/screens/Home/HomeScreen';
+// import Home from './src/screens/Home/HomeScreen';
+ import CartScreen from './src/screens/Cart/CartScreen';
 
 export default function App() {
   return (
-    <Home/>
+    // <Home/>
+    <CartScreen/>
  );
 }
 
