a6a038d9084da7ee17b3e29dd02fc8c495e8f4b8
Harindi Weerasinghe <125258141+harindiw@users.noreply.github.com>
2023-04-05 23:57:52 +0530

    Item

--- WeDet-frontend/App.js
+++ WeDet-frontend/App.js
@@ -1,12 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
+//import HomeScreen from './HomeScreen';
+import WeedResults from './src/screens/Item/WeedResults';
+//import AboutUs from './src/screens/About/AboutUs';
 
-//import your screen
-import Home from './src/screens/Home/HomeScreen';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <Home/>
- );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='WeedResults'
+          component={WeedResults}
+          options={{
+            headerTitle:'Weed Results',
+            headerTintColor:'#01B763',
+            headerTitleAlign:'center'
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+
+       //<AboutUs/>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
